6f758c0e1cbd4f5fa3b9bde9320a135c5d9b5ebe
Jan Sabbe <648689+jansabbe@users.noreply.github.com>
2024-11-11 12:45:11 +0100

    fix basename temporarily

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "http://jansabbe.github.io",
+    site: "http://www.atrenko.com",
     base: "ideal-garbanzo",
 });
